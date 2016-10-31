318b0b61c12da37aa4b0616ea56cf1b559577b21
Andrew Briscoe <admin@pandasportal.net>
2016-11-01 00:07:14 +0800

    Added front end - base

--- iot-mapper-serverless-stack/handler.js
+++ iot-mapper-serverless-stack/handler.js
@@ -1,11 +1,8 @@
 'use strict';
 
 // Your first function handler
-module.exports.helloFunc = (event, context, cb) => {
+module.exports.hello = (event, context, cb) => {
   cb(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
 
 // You can add more handlers here, and reference them in serverless.yml
-modules.exports.getLocation = (event, context,cb) => {
- cb(null, { deviceId: 'test', payload: { lat: 1.0, lng: 0.5 }, event});
-};
