bd0b801a89d93ca688adc1b50e95d241a9fe8ec8
Andrew Briscoe <admin@pandasportal.net>
2016-11-01 05:25:53 +0800

    Code to set bounding map box

--- app-frontend/js/app.js
+++ app-frontend/js/app.js
@@ -36,8 +36,39 @@ app.controller("TestCtrl",["$scope","DeviceData","$interval",function ($scope,De
             $scope.deviceAttrs[i] = {deviceId: d.deviceId, lat: move(d.lat), lng: move(d.lng)};
           }
         };
-        //$interval(randomMove,100,10000);
+        //$interval(randomMove,500,10000);
         // Need to add remove
         // Need to add update
+        
+
+        // This code comes from:
+        // http://stackoverflow.com/questions/6048975/google-maps-v3-how-to-calculate-the-zoom-level-for-a-given-bounds
+        /*function getBoundsZoomLevel(bounds, mapDim) {
+	    var WORLD_DIM = { height: 256, width: 256 };
+	    var ZOOM_MAX = 21;
+
+	    function latRad(lat) {
+		var sin = Math.sin(lat * Math.PI / 180);
+		var radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
+		return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
+	    }
+
+	    function zoom(mapPx, worldPx, fraction) {
+		return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
+	    }
+
+	    var ne = bounds.getNorthEast();
+	    var sw = bounds.getSouthWest();
+
+	    var latFraction = (latRad(ne.lat()) - latRad(sw.lat())) / Math.PI;
+
+	    var lngDiff = ne.lng() - sw.lng();
+	    var lngFraction = ((lngDiff < 0) ? (lngDiff + 360) : lngDiff) / 360;
+
+	    var latZoom = zoom(mapDim.height, WORLD_DIM.height, latFraction);
+	    var lngZoom = zoom(mapDim.width, WORLD_DIM.width, lngFraction);
+
+	    return Math.min(latZoom, lngZoom, ZOOM_MAX);
+	}*/
         
 }]);
