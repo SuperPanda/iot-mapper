a05a3ceacec2bc5d0a3a5936374eaf364d79ecae
Andrew Briscoe <admin@pandasportal.net>
2016-11-07 06:32:19 +0800

    Updated IoT device removal script

--- device-simulator/remove-a-device.sh
+++ device-simulator/remove-a-device.sh
@@ -1,18 +1,31 @@
+# This helper script requires the AWS CLI to be configured correctly,
+# and requires a program called jq. See GitHub wiki sidebar for more info
+
 certificateId=`aws iot list-certificates | jq  -r '.certificates[0].certificateId'`
 certificateARN=`aws iot list-certificates | jq -r '.certificates[0].certificateArn'`
+
 policyName="Thing_Policy"
-# while [ "$certificateId" != "null" ]; do
-#aws iot list-principal-policies --certificateARN
-#aws iot detach-principal-policy --policy-name "" --principle certificateARN
-# remove policy from certificate
+
+thingNamePrefix="MyThing"
+thingNameSuffix=${certificateId:0:16}
+thingName="${thingNamePrefix}-${thingNameSuffix}"
+
+# remove policy from device certificate
+echo "Detaching the principal/certificate from the device"
+aws iot detach-thing-principal --thing-name $thingName --principal $certificateARN
+
+# delete thing from IoT registry
+echo "Deleting thing"
+aws iot delete-thing --thing-name $thingName
+
+# detaching the policy from the device certificate
 echo "Removing $policyName from certificate $certificateId"
 aws iot detach-principal-policy --policy-name $policyName --principal $certificateARN
-# deactive certificate
+
+# deactivate the device certificate
 echo "Deactivating certificate $certificateId"
 aws iot update-certificate --certificate-id $certificateId --new-status INACTIVE
-# remove certificate
+
+# remove the device certificate from AWS IoT
 echo "Deleting certificate $certificateId"
 aws iot delete-certificate --certificate-id $certificateId
-#$certificateId=`aws iot list-certificates | jq '.certificates[0].certificateId'`
-#$certificateARN=`aws iot list-certificates | jq '.certificates[0].cerificateArn'`
-#done
