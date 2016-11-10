1212f9d126ab5423a5c3ae7242bbb2a735c8f7e8
Andrew Briscoe <admin@pandasportal.net>
2016-11-11 04:04:02 +0800

    Still developing

--- iot-map-frontend/js/app - Copy.js
+++ iot-map-frontend/js/app - Copy.js
@@ -1,21 +1,17 @@
 var app = angular.module('ng-iot',['ng-iot.directives','ngRoute', 'ngMap','ngWebSocket'])
 
 
-//var GOOGLE_API_KEY = "AIzaSyBeUahQKqgnvFoRhM3PVnZ_gp-2C7fFQm0";
-//var CLIENT_ID="679783015761-140qrdlv52natr5k0i2isn8mglnedb3v.apps.googleusercontent.com";
-//var CLIENT_SECRET="UgqR_i8Ws-gc_r6cikzvHJng";
+var GOOGLE_API_KEY = "AIzaSyBeUahQKqgnvFoRhM3PVnZ_gp-2C7fFQm0";
+var CLIENT_ID="679783015761-140qrdlv52natr5k0i2isn8mglnedb3v.apps.googleusercontent.com";
+var CLIENT_SECRET="UgqR_i8Ws-gc_r6cikzvHJng";
 .config(function($routeProvider) {
   $routeProvider
-  .when('/login', {
+  .when('/', {
     controller: 'MainCtrl',
     templateUrl: 'templates/main.html',
   })
-  .when('/login',{
-		controller: 'MainCtrl',
-		templateUrl: 'templates/main.html'
-  })
   .otherwise({
-    redirectTo: '/login'
+    redirectTo: '/'
   });
 });
 window.onLoadCallback = function() {
@@ -28,7 +24,6 @@ window.onLoadCallback = function() {
     });
   });
 }
-  
 app.factory('DeviceData',function(){
   return {
    getDevices: function getDevices(x,y,r){
@@ -53,42 +48,38 @@ app.factory('iotData', function($websocket){
     }
   };
   return methods;
-});
+}
 
 // See angularjs-google-maps for example of a good test suite
-app.controller("MainCtrl",["$scope","DeviceData","iotData",function ($scope,DeviceData,iotData){
-	$scope.signedIn = function(oauth) {
-		console.log("Signed In");
-      $scope.user = oauth;
-    }
-     //   $scope.iotData = iotData;
-        /*$scope.currentDeviceIds = [];
+app.controller("TestCtrl",["$scope","DeviceData","iotData",function ($scope,DeviceData,iotData){
+        $scope.iotData = iotData;
+        $scope.currentDeviceIds = [];
         $scope.deviceAttrs = [];
         $scope.viewRadius = 10; // arbitrary
         $scope.position = {lat: 5, lng: 5}; //blah
         $scope.numOfDevices = 0; 
-	      $scope.testValue = 123;*/
+	      $scope.testValue = 123;
         //var parseDevices = function(data){
           //$scope.numOfDevices = data.length;
         //}
         //DeviceData.getDevices(1,2,3).success(parseDevices); 
 
-        /*var devices = DeviceData.getDevices(1,2,3);
+        var devices = DeviceData.getDevices(1,2,3);
         for (i = 0; i < devices.length; i++){
           // Add if not exist
           if (!(devices[i].deviceId in $scope.currentDeviceIds)){
             $scope.currentDeviceIds.push(devices[i].deviceId);
             $scope.deviceAttrs.push(devices[i]);           
           }
-        }*/
-    /*
+        }
+    
         var randomMove = function(){
       	  var move = function(pos){ return pos + (Math.random()*2.0-1.0); };
           for (i = 0; i < devices.length; i++){
             var d = $scope.deviceAttrs[i];
             $scope.deviceAttrs[i] = {deviceId: d.deviceId, lat: move(d.lat), lng: move(d.lng)};
           }
-        };*/
+        };
         // FOR CODE ON CONTROLLING THE ZOOM LEVEL BASED ON BOUNDS
         // REVIEW COMMIT LOG BEFORE 10 NOVEMBER
         // AND LOOK AT IMPLEMENTATING LEAFLET JS INSTEAD
