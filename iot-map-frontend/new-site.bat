ec02f9c13301a331d3546aa6787f379506747781
Andrew Briscoe <admin@pandasportal.net>
2016-11-11 20:06:00 +0800

    Got the Front-end to provide a simulated experience of the interface. All the is let to do is make the websocket connections, and populate the array od models.

--- iot-map-frontend/new-site.bat
+++ iot-map-frontend/new-site.bat
@@ -1,9 +1,11 @@
-aws s3 cp index-working-copy.html s3://iot.pandasportal.net/index.html
-aws s3 cp js/app.js s3://iot.pandasportal.net/scripts/app.js
-aws s3 cp js/directives.js s3://iot.pandasportal.net/scripts/directives.js
-aws s3 cp template/main.html s3://iot.pandasportal.net/templates/main.html
-aws s3 cp template/map.html s3://iot.pandasportal.net/templates/map.html
-aws s3 cp js/services.js s3://iot.pandasportal.net/scripts/services.js
 
-aws s3 cp bower_components/angular-websocket/dist/angular-websocket.min.js s3://iot.pandasportal.net/vendor/angular-websocket/angular-websocket.min.js
-aws s3 cp bower_components/ngmap/build/scripts/ng-map.min.js s3://iot.pandasportal.net/vendor/ngmap/ng-map.min.js
+aws s3 cp index.html s3://iot.pandasportal.net/index.html
+
+aws s3 cp scripts/app.js s3://iot.pandasportal.net/scripts/app.js
+aws s3 cp scripts/services.js s3://iot.pandasportal.net/scripts/services.js
+aws s3 cp scripts/directives.js s3://iot.pandasportal.net/scripts/directives.js
+aws s3 cp templates/main.html s3://iot.pandasportal.net/templates/main.html
+aws s3 cp templates/map.html s3://iot.pandasportal.net/templates/map.html
+
+aws s3 cp vendor/angular-websocket/angular-websocket.min.js s3://iot.pandasportal.net/vendor/angular-websocket/angular-websocket.min.js
+aws s3 cp vendor/ngmap/ng-map.min.js s3://iot.pandasportal.net/vendor/ngmap/ng-map.min.js
